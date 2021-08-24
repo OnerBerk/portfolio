1b1f5b0dc3992cad2390118b9a53b395b60dead1
OnerBerk <oner.berk@joza-it.fr>
2021-08-24 21:55:31 +0200

    circle progres bar

--- portfolio/pages/competence.js
+++ portfolio/pages/competence.js
@@ -1,16 +1,19 @@
-import React from "react"
-import Progress from "../component/progress-bar"
+import React,{useEffect,useState} from "react"
 import Link from "next/link"
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faAngleDoubleRight} from "@fortawesome/free-solid-svg-icons";
 import {faHome} from "@fortawesome/free-solid-svg-icons";
 import Head from "next/head";
 
-import { motion } from 'framer-motion';
+import {motion} from 'framer-motion';
 import pageVariants from "../component/motionVariant/variant";
 import styles from "../styles/competence.module.scss";
 
+import {CircularProgressbarWithChildren, buildStyles} from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
+
 const Competence = () => {
+
     return (
         <motion.div
             initial="initial"
@@ -18,32 +21,155 @@ const Competence = () => {
             exit="out"
             variants={pageVariants}
         >
-        <div className={styles.compContainer}>
-            <Head>
-                <title>Oner Berk Portfolio Compétences </title>
-                <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-            </Head>
-            <div className={styles.iconCont}>
-                <Link href="/"><i><FontAwesomeIcon className={styles.icon} icon={faHome}/></i></Link>
-                <Link href="/projet"><i><FontAwesomeIcon className={styles.icon} icon={faAngleDoubleRight}/></i></Link>
-            </div>
+            <div className={styles.compContainer}>
+                <Head>
+                    <title>Oner Berk Portfolio Compétences </title>
+                    <meta name="viewport" content="initial-scale=1.0, width=device-width"/>
+                </Head>
+                <div className={styles.iconCont}>
+                    <Link href="/"><i><FontAwesomeIcon className={styles.icon} icon={faHome}/></i></Link>
+                    <Link href="/projet"><i><FontAwesomeIcon className={styles.icon}
+                                                             icon={faAngleDoubleRight}/></i></Link>
+                </div>
                 <h1> Compétences<span>.Tech()</span></h1>
 
-            <div className={styles.compContent}>
-                <div className={styles.progressglobal}>
-                    <div className={styles.progressBlock}><Progress done={90} color={"#f8a839"} title={"< Html > Almost "}/></div><br/>
-                    <div className={styles.progressBlock}><Progress done={85}color={"#399BF8"} title={"#Css - .Scss "}/></div><br/>
-                    <div className={styles.progressBlock}><Progress done={75} color={"#57D94D "} title={"NodeJs"}/></div><br/>
-                    <div className={styles.progressBlock}><Progress done={60} color={"#efcb98"} title={"Python"}/></div><br/>
-                    <div className={styles.progressBlock}><Progress done={45} color={"#b864ec"} title={"PHP"}/></div><br/>
-                    <div className={styles.progressBlock}><Progress done={85} color={"#90F7FB "} title={"○ ○ ○ ReactJs "}/></div><br/>
-                    <div className={styles.progressBlock}><Progress done={85} color={"snow"} title={"•NextJs "}/></div><br/>
-                    <div className={styles.progressBlock}><Progress done={75} color={"#10AC26"} title={"MongoDb"}/></div><br/>
-                    <div className={styles.progressBlock}><Progress done={45} color={"#EA3B08"} title={"MySql"}/></div><br/>
-                    <div className={styles.progressBlock}><Progress done={70} color={"#F5F27D "} title={"Git"}/></div><br/>
-                </div><br/>
-            </div><br/>
-        </div>
+                <div className={styles.compContent}>
+                    <div className={styles.progressglobal}>
+                        <div style={{width: 150}}>
+                            <p>HTML</p>
+                            <CircularProgressbarWithChildren
+                                value={90}
+                                styles={buildStyles({
+                                    pathColor: "#e06631",
+                                    trailColor: "gold"
+                                })}
+                            >
+                                <img style={{width: 80}} src="/html-5.png" alt="html-logo"/>
+                            </CircularProgressbarWithChildren>
+                        </div>
+                        <div style={{width: 150}}>
+                            <p>CSS</p>
+                            <CircularProgressbarWithChildren
+                                value={85}
+                                styles={buildStyles({
+                                    pathColor: "#96d6e6",
+                                    trailColor: "gold"
+                                })}
+                            >
+                                <img style={{width: 80}} src="/css.png" alt="css-logo"/>
+                            </CircularProgressbarWithChildren>
+                        </div>
+                        <div style={{width: 150}}>
+                            <p>JAVASCRIPT</p>
+                            <CircularProgressbarWithChildren
+                                value={65}
+                                styles={buildStyles({
+                                    pathColor: "snow",
+                                    trailColor: "gold"
+                                })}
+                            >
+                                <img style={{width: 80}} src="/javascript.png" alt="javascript logo"/>
+                            </CircularProgressbarWithChildren>
+                        </div>
+                        <div style={{width: 150}}>
+                            <p>NODE JS</p>
+                            <CircularProgressbarWithChildren
+                                value={75}
+                                styles={buildStyles({
+                                    pathColor: "#0bc247",
+                                    trailColor: "gold"
+                                })}
+                            >
+                                <img style={{width: 78}} src="/nodeJs.png" alt="nodeJs logo"/>
+                            </CircularProgressbarWithChildren>
+                        </div>
+                        <div style={{width: 150}}>
+                            <p>REACT JS</p>
+                            <CircularProgressbarWithChildren
+                                value={85}
+                                styles={buildStyles({
+                                    pathColor: "#61bfeb",
+                                    trailColor: "gold"
+                                })}
+                            >
+                                <img style={{width: 98}} src="/react.png" alt="react js logo"/>
+                            </CircularProgressbarWithChildren>
+                        </div>
+                        <div style={{width: 150}}>
+                            <p>NEXT JS</p>
+                            <CircularProgressbarWithChildren
+                                value={75}
+                                styles={buildStyles({
+                                    pathColor: "#00090d",
+                                    trailColor: "gold"
+                                })}
+                            >
+                                <img style={{width: 98}} src="/next-js.png" alt="next js logo"/>
+                            </CircularProgressbarWithChildren>
+                        </div>
+                        <div style={{width: 150}}>
+                            <p>GRAPHQL</p>
+                            <CircularProgressbarWithChildren
+                                value={70}
+                                styles={buildStyles({
+                                    pathColor: "#940a6f",
+                                    trailColor: "gold"
+                                })}
+                            >
+                                <img style={{width: 98, marginLeft: 5}} src="/graphql.png" alt="graphql logo"/>
+                            </CircularProgressbarWithChildren>
+                        </div>
+                        <div style={{width: 150}}>
+                            <p>POSTRESQL</p>
+                            <CircularProgressbarWithChildren
+                                value={65}
+                                styles={buildStyles({
+                                    pathColor: "#4473b9",
+                                    trailColor: "gold"
+                                })}
+                            >
+                                <img style={{width: 85}} src="/psql.png" alt="psql logo"/>
+                            </CircularProgressbarWithChildren>
+                        </div>
+                        <div style={{width: 150}}>
+                            <p>MONGODB</p>
+                            <CircularProgressbarWithChildren
+                                value={85}
+                                styles={buildStyles({
+                                    pathColor: "black",
+                                    trailColor: "gold"
+                                })}
+                            >
+                                <img style={{width: 35}} src="/mongoDb.png" alt="graphql logo"/>
+                            </CircularProgressbarWithChildren>
+                        </div>
+                        <div style={{width: 150}}>
+                            <p>DOCKER</p>
+                            <CircularProgressbarWithChildren
+                                value={55}
+                                styles={buildStyles({
+                                    pathColor: "#4473b9",
+                                    trailColor: "gold"
+                                })}>
+                                <img style={{width: 95}} src="/docker.png" alt="docker-logo"/>
+                            </CircularProgressbarWithChildren>
+                        </div>
+                        <div style={{width: 150}}>
+                            <p>GIT</p>
+                            <CircularProgressbarWithChildren
+                                value={75}
+                                styles={buildStyles({
+                                    pathColor: "#ce4c13",
+                                    trailColor: "gold"
+                                })}>
+                                <img style={{width: 98}} src="/git.png" alt="docker-logo"/>
+                            </CircularProgressbarWithChildren>
+                        </div>
+                    </div>
+                    <br/>
+                </div>
+                <br/>
+            </div>
         </motion.div>
     )
 }
