3a1a748a28585107b5a38ee94a32e68d7b8f0b72
OnerBerk <oner.berk@joza-it.fr>
2022-11-26 23:24:41 +0100

    stamp done

--- portfolio/pages/stamp.tsx
+++ portfolio/pages/stamp.tsx
@@ -2,55 +2,43 @@ import styles from "../styles/contact.module.scss";
 import Layout from "../component/layout/layout";
 import {useSelector} from "../store/store";
 import {getTheme} from "../store/slices/theme/theme.slice";
-import {faGripLines} from "@fortawesome/free-solid-svg-icons";
 import {faStar} from "@fortawesome/free-solid-svg-icons";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
 
 const Stamp = () => {
-  const {bgColor, primaryColor, secondaryColor} = useSelector(getTheme);
+  const {primaryColor} = useSelector(getTheme);
   return (
     <Layout title="contact">
       <div className={styles.contactMain}>
         <div style={{
-          border: "16px",
+          border: "12px",
           borderStyle: "solid",
           borderColor: primaryColor
         }} className={styles.contactBody}>
-          <div className={styles.contactLeft}
-               style={{
-                 borderRight: 10,
-                 borderRightStyle: "solid",
-                 borderRightColor: primaryColor
-               }}>
-            <div className={styles.contactleftTop}>
-              Identity
-            </div>
-            <div className={styles.contactLeftBot}>#23</div>
-          </div>
           <div className={styles.contactRight}>
-            <div>devloppeur fullstack</div>
-            <div className={styles.contactRightDividor}>
+            <div className={styles.contactTitle}>Dévloppeur fullstack</div>
+            <div  className={styles.contactRightDividor}>
               <span style={{
-                borderBottom:10,
-                borderBottomStyle:"solid",
-                borderBottomColor: primaryColor}} />
+                borderBottom: 10,
+                borderBottomStyle: "solid",
+                borderBottomColor: primaryColor
+              }} />
               <div>
                 <FontAwesomeIcon className={styles.socialIcon}
                                  style={{color: primaryColor}}
                                  icon={faStar} />
               </div>
               <span style={{
-                borderBottom:10,
-                borderBottomStyle:"solid",
-                borderBottomColor: primaryColor}} />
+                borderBottom: 10,
+                borderBottomStyle: "solid",
+                borderBottomColor: primaryColor
+              }} />
             </div>
             <div className={styles.contactName}>
-            <div className={styles.lastname}>ÖNER</div>
-            <div className={styles.firstname}>
-              BERK
-            </div>
-            <div></div>
+              <div className={styles.lastname}>ÖNER</div>
+              <div className={styles.firstname}>BERK</div>
+              <div></div>
             </div>
           </div>
         </div>
