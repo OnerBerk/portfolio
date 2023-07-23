475668fff8220d15b6a4d7fdacd037ae70c49ae4
OnerBerk <oner.berk@joza-it.fr>
2023-07-23 19:04:08 +0200

    layout

--- portfolio/component/layout/layout.tsx
+++ portfolio/component/layout/layout.tsx
@@ -218,7 +218,7 @@ const Layout = ({children, title}: LayoutProps) => {
       <Head>
         <title>{title}</title>
       </Head>
-      {skills && <div className={styles.layoutHeader}>
+      {!skills && <div className={styles.layoutHeader}>
         <div className={styles.headerLeft}>
           <div>Öner Berk</div>
         </div>
