2963571c9c098479a69570e8c47baff16b63ab7a
OnerBerk <oner.berk@joza-it.fr>
2023-11-11 15:35:29 +0100

    theme

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,14 +1,22 @@
 import "./layout.scss";
-import {ReactNode} from "react";
+import {Dispatch, ReactNode, SetStateAction, useEffect} from "react";
 import darkDot from "../../assets/dot.svg";
 import lightDot from "../../assets/light-dot.svg";
 import yellowDot from "../../assets/yellow-dot.svg";
+import {ThemeEnum} from "../../domain/domain";
 
-const Layout = ({children}: {children: ReactNode}) => {
-  var symboleCopyright = "\u00A9";
+type LayoutProps = {
+  children: ReactNode;
+  theme: ThemeEnum;
+  setTheme: Dispatch<SetStateAction<ThemeEnum>>;
+};
+const Layout = ({children, theme, setTheme}: LayoutProps) => {
+  const symboleCopyright = "\u00A9";
   const year = new Date().getFullYear();
+  const returnTheme = () => theme;
+
   return (
-    <div className="layout-main">
+    <div className={`layout-main ${returnTheme()}`}>
       <div className="l">
         <div className="left-block">
           <div className="ligne" />
@@ -21,9 +29,21 @@ const Layout = ({children}: {children: ReactNode}) => {
           <div className="top-ligne" />
           <div className="top-right">
             <span>Öner</span>
-            <img alt="dark theme" src={darkDot} />
-            <img alt="light theme" src={lightDot} />
-            <img alt="yellow theme" src={yellowDot} />
+            <img
+              onClick={() => setTheme(ThemeEnum.dark)}
+              alt="dark theme"
+              src={darkDot}
+            />
+            <img
+              onClick={() => setTheme(ThemeEnum.light)}
+              alt="light theme"
+              src={lightDot}
+            />
+            <img
+              onClick={() => setTheme(ThemeEnum.yellow)}
+              alt="yellow theme"
+              src={yellowDot}
+            />
           </div>
         </div>
       </div>
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,8 +1,14 @@
-import React from "react";
+import React, {useState} from "react";
 import Layout from "../components/layout/layout";
+import {ThemeEnum} from "../domain/domain";
 
 function App() {
-  return <Layout>Bonjour</Layout>;
+  const [theme, setTheme] = useState<ThemeEnum>(ThemeEnum.dark);
+  return (
+    <Layout setTheme={setTheme} theme={theme}>
+      Bonjour
+    </Layout>
+  );
 }
 
 export default App;
