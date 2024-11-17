dda2281e0482d37f078d01724d5f34038a00d00d
OnerBerk <oner_berk@hotmail.com>
2024-11-17 19:03:14 +0100

    clean

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,30 @@
-import React, {useCallback} from "react";
+import React, {useCallback} from 'react';
 
-import {RouterProvider} from "react-router-dom";
-import {router} from "./pages/router/router";
-import {IntlProvider} from "react-intl";
+import {RouterProvider} from 'react-router-dom';
+import {router} from './pages/router/router';
+import {IntlProvider} from 'react-intl';
 
-import { SpeedInsights } from '@vercel/speed-insights/react';
-import {RootState} from "./redux/store/store";
-import {useSelector} from "react-redux";
+import {SpeedInsights} from '@vercel/speed-insights/react';
+import {RootState} from './redux/store/store';
+import {useSelector} from 'react-redux';
 
-import {Lang} from "./domain/domain";
+import {Lang} from './domain/domain';
 
-import enMessages from "./lang/en.json";
-import frMessages from "./lang/fr.json";
+import enMessages from './lang/en.json';
+import frMessages from './lang/fr.json';
 
 function App() {
-  const locale = "fr";
+  const locale = 'fr';
   const lang = useSelector((state: RootState) => state.lang?.lang);
 
   const messages = useCallback(() => {
     return lang === Lang.en ? enMessages : frMessages;
   }, [lang]);
   return (
-      <IntlProvider locale={locale} messages={messages()}>
-        <RouterProvider router={router} />
-        <SpeedInsights />
-      </IntlProvider>
+    <IntlProvider locale={locale} messages={messages()}>
+      <RouterProvider router={router} />
+      <SpeedInsights />
+    </IntlProvider>
   );
 }
 export default App;
