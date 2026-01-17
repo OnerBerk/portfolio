c40a698fe42a1a9cbe2779a27e631cff710fa1a9
Berk Oner <oner_berk@hotmail.com>
2026-01-17 17:37:34 +0100

    projects

--- src/pages/projects/projects.tsx
+++ src/pages/projects/projects.tsx
@@ -1,19 +1,79 @@
-import {Typography} from '@mui/material';
+import {Box, Grid, Stack, Typography, useMediaQuery, useTheme, Link} from '@mui/material';
 import PageLayout from '../../components/layout/page-layout/page-layout';
 import {defineMessages, useIntl} from 'react-intl';
+import bow from '@/assets/bow.png';
+import {HEAR_ME_OUT_LINK} from '@/utils/links';
 
 const messages = defineMessages({
-  wip: {
-    defaultMessage: 'Work in progress',
-    id: 'projects.wip',
+  title: {
+    defaultMessage: 'Projects',
+    id: 'projects.title',
   },
 });
 
+interface Iproject {
+  name: string;
+  description: string;
+  image: string;
+  link: string;
+}
+
+const projectsList: Iproject[] = [
+  {
+    name: 'Hear me out',
+    description: 'A platform for finding and listening to podcasts',
+    image: bow,
+    link: HEAR_ME_OUT_LINK,
+  },
+];
+
 const Projects = () => {
   const intl = useIntl();
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down('md'));
   return (
     <PageLayout>
-      <Typography>{intl.formatMessage(messages.wip)}</Typography>
+      <Stack alignItems='center' justifyContent='center' height='100%' width='100%' zIndex={1}>
+        <Box marginTop={0} marginBottom='auto'>
+          <Typography variant='h1' sx={{fontSize: isMobile ? '4.5rem' : '6.5rem', fontWeight: 'semibold'}}>
+            {intl.formatMessage(messages.title)}
+          </Typography>
+        </Box>
+        <Grid
+          fontFamily='Merriweather, serif'
+          container
+          width='100%'
+          height='100%'
+          alignItems='center'
+          justifyContent='center'
+          px={5}>
+          <Grid item>
+            {projectsList.map((project) => (
+              <Grid
+                item
+                key={project.name}
+                border={1}
+                width={isMobile ? '60%' : '30%'}
+                bgcolor='rgba(255, 255, 255, 0.7)'
+                borderRadius={3}>
+                <Link href={project.link} target='_blank' rel='noopener noreferrer' sx={{textDecoration: 'none'}}>
+                  <Box sx={{zIndex: 1}}>
+                    <img src={project.image} alt={project.name} width='100%' height='100%' />
+                    <Typography
+                      borderTop={1}
+                      fontSize={isMobile ? '1.5rem' : '2rem'}
+                      textAlign='center'
+                      fontWeight='700'
+                      sx={{color: '#000'}}>
+                      {project.name}
+                    </Typography>
+                  </Box>
+                </Link>
+              </Grid>
+            ))}
+          </Grid>
+        </Grid>
+      </Stack>
     </PageLayout>
   );
 };
---
+++ src/utils/links.ts
@@ -0,0 +1,1 @@
+export const HEAR_ME_OUT_LINK = 'https://hear-me-8bjhga28k-onairs-projects.vercel.app/';
